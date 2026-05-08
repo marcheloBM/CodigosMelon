9ec2c0614753d1b2a687cb5325b9196b740f8f8d
marchelo <marchelo.bm@gmail.com>
2026-05-08 02:58:57 -0400

    v4.1.4 Actualizacion del Codigo
    Gradle 9.1.0 a 9.2.0

--- CodigosMelon/app/build.gradle.kts
+++ CodigosMelon/app/build.gradle.kts
@@ -1,35 +1,46 @@
 plugins {
     id("com.android.application")
-
 }
 
 android {
     namespace = "com.Burgos.codigosmelon"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.Burgos.codigosmelon"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "4.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildFeatures {
         viewBinding = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
     implementation("androidx.sqlite:sqlite:2.3.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation(libs.androidx.monitor)
-    implementation(libs.androidx.junit.ktx)
-    testImplementation(kotlin("test"))
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.junit.ktx)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.monitor)
 }
